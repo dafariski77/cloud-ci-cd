f3a097624893f6f92b35cee8feea1242eb699d31
dafariski77 <rskdafa@gmail.com>
2024-05-31 01:09:07 +0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Example app listening on port!`)
+);
